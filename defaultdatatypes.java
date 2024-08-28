09ec0f94b1016e768e89a489710a6b7411ae2566
peramdurgashankar18 <shankar0864213479@gmail.com>
2024-08-28 10:59:30 +0530

    Create defaultdatatypes.java

---
+++ defaultdatatypes.java
@@ -0,0 +1,22 @@
+//write a program for to display the default value 
+	public class myfirstprogram{
+			static int Int;
+			static float Float;
+			static boolean Boolean;
+			static char Char;
+			static double Double;
+			static long Long;
+			static byte Byte;
+			public static void main(String[] args){
+				System.out.println("default int:" + Int );
+				System.out.println("default float" + Float);
+				System.out.println("default boolean" + Boolean);
+				System.out.println("default char" + Char);
+				System.out.println("default double" + Double);
+				System.out.println("default long" + Long);
+				System.out.println("default byte" + Byte);
+			
+			}
+			
+	
+	}
