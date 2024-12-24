08bfe418c080fd143d1b86b2efe6d560135f32ad
p.shankar <shankar0864213479@gmail.com>
2024-12-24 11:18:57 +0530

    Update bubblesort.java

--- bubblesort.java
+++ bubblesort.java
@@ -1,28 +1,28 @@
 //write a program for sorting of elements in java
 
-import java.util.Scanner;
-public class bubblesort{
-	public static void main(String[] args){
-		int i,n,j,t;
-		Scanner sc=new Scanner(System.in);
-		int[] a=new int[10];
-		System.out.println("enter the array size");
-		n=sc.nextInt();
-		System.out.println("enter the elements in the array");
-		for(i=0;i<n;i++) a[i]=sc.nextInt();
-		for(i=0;i<=n-1;i++){
-			for(j=0;j<=n-i-1;j++){
-				if(a[j]>a[j+1])
-				{
-					t=a[j];
-					a[j]=a[j+1];
-					a[j+1]=t;	
-				}
-				for(i=0;i<=n;i++)
-				System.out.println(a[i]+" ");
+import java.util.*;
+class bubble
+{
+ public static void main(String[] args)
+ {
+	int i,j,temp;
+	int a[]={4,5,7,2,3};
+	for(i=0;i<5;i++)
+	{
+		for(j=0;j<5-i-1;j++)
+		{	
+			if(a[j]>a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
 			}
 		}
-				
-	
+	}
+	for(i=0;i<5;i++)
+	{
+		System.out.println(a[i]);
 	}
 }
+}	
+			
