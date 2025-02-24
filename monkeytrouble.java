c73193b3234cc7f2d4bf31bced764f18fa7e6b96
PERAM DURGA SHANKAR <shankar0864213479@gmail.com>
2025-02-24 18:03:52 +0530

    Create monkeytrouble.java

---
+++ monkeytrouble.java
@@ -0,0 +1,13 @@
+public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+  if (aSmile && bSmile) {
+    return true;
+  }
+  if (!aSmile && !bSmile) {
+    return true;
+  }
+  return false;
+  // The above can be shortened to:
+  //   return ((aSmile && bSmile) || (!aSmile && !bSmile));
+  // Or this very short version (think about how this is the same as the above)
+  //   return (aSmile == bSmile);
+}
