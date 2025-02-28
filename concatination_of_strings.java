4fd48e0eccf67691fa436ac504e800968f51372a
PERAM DURGA SHANKAR <shankar0864213479@gmail.com>
2025-02-28 21:11:15 +0530

    Create concatenation_of_strings.java

---
+++ concatination_of_strings.java
@@ -0,0 +1,8 @@
+public class StringExample{
+    public static void main(String[] args){
+        String str1 = "durga";
+        String str2 = "shankar";
+        String result = str1 + " " + str2;
+        System.out.println(result);
+    }
+}
